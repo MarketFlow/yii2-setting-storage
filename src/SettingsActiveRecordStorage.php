a618c2090fb5e7576e8dd8afc7adf82256cdb37e
Joey Claessen <joey_claessen@hotmail.com>
2016-12-08 12:33:21 +0000

    working v0.1

--- src/SettingsActiveRecordStorage.php
+++ src/SettingsActiveRecordStorage.php
@@ -10,51 +10,54 @@ use yii\db\ActiveRecord;
 class SettingsActiveRecordStorage extends Object implements StorageInterface
 {
     /** @var ActiveRecord */
-    public $userSettingClass;
-    public $userSettingId;
-    public $userSettingIdPrefix = '';
-    public $userSettingValue;
+    public $userClass;
+    public $userId;
+    public $userPrefix = '';
+    public $userValue;
+    public $userScenario = 'default';
 
     /** @var ActiveRecord */
     public $settingClass;
     public $settingId;
-    public $settingIdPrefix = '';
+    public $settingPrefix = '';
     public $settingValue;
+    public $settingScenario = 'default';
 
     /** @var ActiveRecord */
-    public $applicationSettingClass;
-    public $applicationSettingId;
-    public $applicationSettingIdPrefix = '';
-    public $applicationSettingValue;
+    public $applicationClass;
+    public $applicationId;
+    public $applicationPrefix = '';
+    public $applicationValue;
+    public $applicationScenario = 'default';
 
     public function init()
     {
         parent::init();
         if (
-            !isset($this->userSettingClass, $this->settingClass, $this->applicationSettingClass)
-            || !is_subclass_of($this->userSettingClass, ActiveRecord::class)
+            !isset($this->userClass, $this->settingClass, $this->applicationClass)
+            || !is_subclass_of($this->userClass, ActiveRecord::class)
             || !is_subclass_of($this->settingClass, ActiveRecord::class)
-            || !is_subclass_of($this->applicationSettingClass, ActiveRecord::class)
+            || !is_subclass_of($this->applicationClass, ActiveRecord::class)
         ) {
             throw new InvalidConfigException('userSettingClass, settingClass and applicationSettingClass must be set and must be instance of ' . ActiveRecord::class);
         }
 
-        if (!isset($this->userSettingId)) {
-            $class = $this->userSettingClass;
+        if (!isset($this->userId)) {
+            $class = $this->userClass;
             $pk = $class::primaryKey();
 
             if (count($pk) != 1) {
                 throw new \UnexpectedValueException('userSettingId must be set or userSettingClass must be a class with a single field primary key');
             }
 
-            $this->userSettingId = $pk[0];
+            $this->userId = $pk[0];
         }
 
-        if (!isset($this->userSettingValue)) {
-            $this->userSettingValue = 'value';
+        if (!isset($this->userValue)) {
+            $this->userValue = 'value';
         }
 
-        if (!isset($this->settingClass)) {
+        if (!isset($this->settingId)) {
             $class = $this->settingClass;
             $pk = $class::primaryKey();
 
@@ -62,69 +65,127 @@ class SettingsActiveRecordStorage extends Object implements StorageInterface
                 throw new \UnexpectedValueException('settingClass must be set or settingClass must be a class with a single field primary key');
             }
 
-            $this->userSettingId = $pk[0];
+            $this->settingId = $pk[0];
         }
 
         if (!isset($this->settingValue)) {
             $this->settingValue = 'value';
         }
 
-        if (!isset($this->applicationSettingClass)) {
-            $class = $this->applicationSettingClass;
+        if (!isset($this->applicationId)) {
+            $class = $this->applicationClass;
             $pk = $class::primaryKey();
 
             if (count($pk) != 1) {
                 throw new \UnexpectedValueException('applicationSettingClass must be set or applicationSettingClass must be a class with a single field primary key');
             }
 
-            $this->userSettingId = $pk[0];
+            $this->applicationId = $pk[0];
         }
 
-        if (!isset($this->applicationSettingValue)) {
-            $this->applicationSettingValue = 'value';
+        if (!isset($this->applicationValue)) {
+            $this->applicationValue = 'value';
         }
+    }
+
+    private function getSettingObject($key)
+    {
+        $class = $this->settingClass;
+        $result = $class::find()
+            ->where([
+                $this->settingId => $this->settingPrefix . $key
+            ])
+            ->one();
+        return $result;
     }
 
     public function getSetting($key)
     {
-        $class = $this->settingClass;
-        return $class::find()
-            ->select($this->settingValue)
-            ->where([
-                $this->settingId => $this->settingIdPrefix . $key
-            ])
-            ->one();
+        return $this->getSettingObject($key)->{$this->settingValue} ?? null;
     }
 
     public function setSetting($key, $value)
     {
-        // TODO: Implement setSetting() method.
+        $setting = $this->getSettingObject($key);
+        if (is_null($setting)) {
+            $class = $this->settingClass;
+            $setting = new $class([
+                $this->settingId => $this->settingPrefix . $key
+            ]);
+        }
+
+        $setting->scenario = $this->settingScenario;
+        $setting->{$this->settingValue} = $value;
+
+        return $setting->save();
+    }
+
+    private function getApplicationSettingObect($key)
+    {
+        $class = $this->applicationClass;
+        $result = $class::find()
+            ->where([
+                $this->applicationId => $this->applicationPrefix . $key
+            ])
+            ->one();
+        return $result;
     }
 
     public function getApplicationSetting($key)
     {
-        $class = $this->applicationSettingClass;
-        return $class::find()
-            ->select($this->applicationSettingValue)
-            ->where([
-                $this->applicationSettingId => $this->applicationSettingIdPrefix . $key
-            ])
-            ->one();
+        return $this->getApplicationSettingObect($key)->{$this->applicationValue} ?? null;
     }
 
     public function setApplicationSetting($key, $value)
     {
-        // TODO: Implement setApplicationSetting() method.
+        $setting = $this->getApplicationSettingObect($key);
+        if (is_null($setting)) {
+            $class = $this->applicationClass;
+            $setting = new $class([
+                $this->applicationId => $this->applicationPrefix . $key
+            ]);
+        }
+
+        $setting->scenario = $this->applicationScenario;
+        $setting->{$this->applicationValue} = $value;
+
+        return $setting->save();
+    }
+
+    private function getUserSettingObject($userId, $key)
+    {
+        $prefix = str_replace('{userId}', $userId, $this->userPrefix);
+
+        $class = $this->userClass;
+        $result = $class::find()
+            ->where([
+                $this->userId => $prefix . $key
+            ])
+            ->one();
+        return $result;
     }
 
     public function getUserSetting($userId, $key)
     {
-        // TODO: Implement getUserSetting() method.
+        return $this->getUserSettingObject($userId, $key)->{$this->userValue} ?? null;
     }
 
     public function setUserSetting($userId, $key, $value)
     {
-        // TODO: Implement setUserSetting() method.
+        $setting = $this->getUserSettingObject($userId, $key);
+
+        if (is_null($setting)) {
+            $prefix = str_replace('{userId}', $userId, $this->userPrefix);
+            $class = $this->userClass;
+            $setting = new $class([
+                $this->userId => $prefix . $key
+            ]);
+        }
+
+        $setting->scenario = $this->userScenario;
+        $setting->{$this->userValue} = $value;
+
+        return $setting->save();
     }
 
 }
